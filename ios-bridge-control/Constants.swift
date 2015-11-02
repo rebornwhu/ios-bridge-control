dee474f0e768eeeda6779d97f9cc6a9c2fb51a93
rebornwhu <rebornwhu@gmail.com>
2015-11-01 20:06:51 -0800

    Save progress

---
+++ ios-bridge-control/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  ios-bridge-control
+//
+//  Created by Xiao Lu on 11/1/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizatoinCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
--- ios-bridge-control/FirstViewController.swift
+++ ios-bridge-control/FirstViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var officerLabel:UILabel!
+    @IBOutlet var authorizationCodeLabel:UILabel!
+    @IBOutlet var rankLabel:UILabel!
+    @IBOutlet var warpDriveLabel:UILabel!
+    @IBOutlet var warpFactorLabel:UILabel!
+    @IBOutlet var favoriteTeaLabel:UILabel!
+    @IBOutlet var favoriteCaptainLabel:UILabel!
+    @IBOutlet var favoriteGadgetLabel:UILabel!
+    @IBOutlet var favoriteAlienLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
