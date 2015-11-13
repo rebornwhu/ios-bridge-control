4c7177c3daa0f97c442325af6503a80f819a85fb
rebornwhu <rebornwhu@gmail.com>
2015-11-12 22:02:45 -0800

    Get settings bundle's values into app when start

--- ios-bridge-control/AppDelegate.swift
+++ ios-bridge-control/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let defaults = [warpDriveKey: true, warpFactorKey: 5, favoriteAlienKey: "Vulcan"]
+        NSUserDefaults.standardUserDefaults().registerDefaults(defaults)
+        
         return true
     }
 
