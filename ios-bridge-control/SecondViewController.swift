f03ee94f8bf46f39032a5170f8d3f7bd570c846e
rebornwhu <rebornwhu@gmail.com>
2015-11-12 21:44:27 -0800

    Build UI for 2nd screen

--- ios-bridge-control/SecondViewController.swift
+++ ios-bridge-control/SecondViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet var engineSwitch:UISwitch!
+    @IBOutlet var warpFactorSlider:UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,13 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func engineSwitchTapped(sender: AnyObject) {
+    }
 
+    @IBAction func warpSliderTouched(sender: AnyObject) {
+    }
+    
+    @IBAction func settingsButtonClicked(sender: AnyObject) {
+    }
 }
 
